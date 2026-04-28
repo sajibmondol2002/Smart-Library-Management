b6d32f57d8da73953606bb333a43d3c577253d32
Md. Sajib Mondol <sajibmondol2002@gmail.com>
2026-04-28 20:09:55 +0600

    Add files via upload

---
+++ SmartLibrary/admin/view/header.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="../css/mystyle.css"> 
+
+<header>
+    <center>
+        <img src="library-logo.jpg" alt="Library Logo" class="logo" height="40" width="40">
+        <div class="header-text">
+            <h1>Smart Library Management</h1>
+            <p>Enhancing Your Reading Experience</p>
+        </div>
+    </center>
+</header>
