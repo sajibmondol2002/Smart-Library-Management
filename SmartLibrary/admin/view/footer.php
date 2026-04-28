a1a6201542f8fd01fd667b3017b548e573f836d7
Md. Sajib Mondol <sajibmondol2002@gmail.com>
2026-04-28 20:08:37 +0600

    Add files via upload

---
+++ SmartLibrary/admin/view/footer.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="../css/mystyle.css"> 
+
+<footer>
+    <center>
+        <div class="footer-content">
+            <h3>Contact</h3>
+            <p>📍 AIUB Library, AIUB Campus, Dhaka</p>
+            <p>☎️ Phone: +8801234567891</p>
+            <p>✉️ E-mail: library@aiub.edu</p>
+        </div>
+        <p class="footer-bottom">Copyright &copy; 2026 - Smart Library Management </p>
+    </center>
+</footer>
